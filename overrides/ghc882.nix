4baf989413bde9a8d9e075c58506e16ce6f5c751
Joe Hermaszewski <git@monoid.al>
2020-03-11 23:58:42 +0800

    Add percentage display patch

--- overrides/ghc882.nix
+++ overrides/ghc882.nix
@@ -5,4 +5,7 @@ self: super: {
   haskell-ide-engine = super.haskell-ide-engine.overrideAttrs (old: {
     patches = (old.patches or []) ++ [../no-bad-diag.patch];
   });
+  hie-plugin-api = super.hie-plugin-api.overrideAttrs (old: {
+    patches = (old.patches or []) ++ [../percent.patch];
+  });
 }
